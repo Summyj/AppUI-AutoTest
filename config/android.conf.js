7ced2598521445f8abc5179c2ff6cb98a45aeb1e
Summyj <18829551943@163.com>
2021-05-17 11:01:02 +0800

    change name again

--- config/android.conf.js
+++ config/android.conf.js
@@ -5,7 +5,7 @@ config.capabilities=[
         platformName: "Android",
         platformVersion: "10.0",
         // deviceName: "Pixel_3_XL_API_29",
-        deviceName: "emulator",
+        deviceName: "Pixel 2",
         automationName: "UiAutomator2",
         // app: "/Users/usr/Documents/AppUI-AutoTest/app/Android-NativeDemoApp-0.2.1.apk"
         app: "/bitrise/src/AppUI-AutoTest/app/Android-NativeDemoApp-0.2.1.apk"
